c15be59563300661acb7d6e215c02d6e0a48e308
Fabian <fabkwak@yahoo.com>
2013-08-20 16:43:06 -0700

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,42 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Golf App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Golf App')
     end
-  end
-end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Golf App | Home")
+    end
+
+  end #home page
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Golf App | Help")
+    end
+  end #help
+
+  describe "About page" do
+
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Golf App | About")
+    end
+  end #help
+
+end #static pages
